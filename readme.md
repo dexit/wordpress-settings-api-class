9090c0c6a7deb38b3c92a6e4e7685dae0d16dd48
Guido W. Pettinari <guido.pettinari@idearia.it>
2021-06-01 11:37:24 +0200

    Aggiunta classe config x accedere alle opzioni serializzate

--- readme.md
+++ readme.md
@@ -1,12 +1,12 @@
 # Come funziona
-Crea nuovi menu o sottomenu in WordPress estendendo la classe `Idearia\SettingsPage`.
 
+* Crea nuovi menu o sottomenu in WordPress estendendo la classe `Idearia\SettingsPage`.
+* Accedi alle opzioni serializzate come fossi in Laravel estendend la classe `Idearia\Config`.
 # Per avere più controllo
 
 La classe `Idearia\SettingsPage` è un wrapper di `Idearia\SettingsApi` che ne semplifica l'utilizzo, che a sua volta è un wrapper della [Settings API di WordPress](https://codex.wordpress.org/Settings_API).
 
 Per avere maggiore controllo sulle opzioni, puoi usare direttamente la classe `Idearia\SettingsApi`.
-
 # Dopo ogni modifica al repo...
 
 Aggiorna il repository composer di Idearia come da istruzioni:
---
+++ src/class-config.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace Idearia;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Accedi alle opzioni serializzate di WordPress usando la sintassi
+ * di Laravel.
+ *
+ * Si tratta di un wrapper di get_option() che permette di
+ * usare la sintassi
+ *   config('sezione.opzione')
+ * per accedere alle opzioni, invece di dover fare
+ *   $sezione_x = get_option('sezione_x)
+ *   $opzione_y = $sezione_x['opzione_y]
+ */
+abstract class Config
+{
+	/**
+	 * Opzionalmente, specifica i valori di default da 
+     * quando non viene trovata un'opzione a database.
+     * 
+     * Esempio:
+     * return [
+	 *		'tavoli.management_mode'  => 'standard',
+	 *		'tavoli.default_staff_id' => null,
+	 *		'impostazioni_ristorante.max_capacity_per_turno' => 50,
+	 *		'impostazioni_sistema.max_giorni_gestione_turni' => 20,
+	 *	];
+     *
+     * NOTA BENE: Volendo qui puoi anche inserire opzioni che non
+     * esistono a database o nelle pagine di settings. È un buon modo
+     * per avere un controllo centralizzato di quelle opzioni che non
+     * hai ancora esposto all'utente.
+	 */
+	public static function getDefaults(): array
+    {
+        return [];
+    }
+
+    /**
+     * Opzionalmente, specifica un prefisso da applicare ai nomi delle
+     * opzioni.
+     *
+     * Ad esempio, se il prefisso è 'test_' chiamando
+     *   config('foo')
+     * verrà ritornata l'opzione test_foo.
+     */
+    protected static $db_prefix = '';
+
+	/**
+	 * Accedi alle opzioni senza curarti di dove sono immagazzinate.
+	 *
+	 * Ad esempio, per accedere all'elemento foo contenuto nell'opzione
+	 * serializzata bar, basta chiamare config( 'bar.foo' ). Per accedere
+	 * all'intera opzione serializzata, basta chiamare config( 'bar' ).
+	 *
+	 * Se l'opzione non esiste, verrà ritornato il valore di default
+	 * dato come secondo argomento; se questi è null, verrà ritornato
+	 * il valore di default definito in getDefaults; se non esiste
+	 * nemmeno questo, verrà ritornato null.
+	 *
+	 * Il modello è quello della funzione config di Laravel:
+	 * https://laravel.com/docs/8.x/helpers#method-config
+	 *
+	 * @throws Exception Quando viene passata una stringa vuota o invalida.
+	 *
+	 * @todo Aggiungi ricorsività
+	 */
+	public static function config( string $query, $default = null )
+	{
+		$tokens = explode( '.', $query );
+
+		$option_name = $tokens[0];
+
+		if ( ! $option_name ) {
+			throw new Exception( 'Passato valore vuoto o invalido a config', 1 );
+		}
+
+		$default = $default ?? self::getDefaultValue( $query );
+
+        $option_key = $option_name;
+
+        if ( self::$db_prefix ) {
+            $option_key = self::$db_prefix . $option_key;
+        }
+        
+        $option_value = get_option( $option_key, $default );
+
+		if ( count( $tokens ) === 1 ) {
+			return $option_value;
+		}
+
+		return $option_value[ $tokens[1] ] ?? $default;
+	}
+
+	/**
+	 * Data un'opzione, ritorna il suo valore di default;
+	 * se non è definito, ritorna null
+	 */
+	public static function getDefaultValue( string $query )
+	{
+		$defaults = self::getDefaults();
+		
+		return $defaults[ $query ] ?? null;
+	}
+}
