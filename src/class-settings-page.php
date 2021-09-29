fa0874e18ef72b345115eb9ffe3c8621d4fe4700
Guido W. Pettinari <guido.pettinari@idearia.it>
2021-09-29 17:12:37 +0200

    Funzione SettingsPage::get_option()

--- src/class-settings-page.php
+++ src/class-settings-page.php
@@ -115,4 +115,24 @@ abstract class SettingsPage extends MenuPage
 		$this->api->show_forms();
 		echo '</div>';
 	}
+
+	/**
+	 * Ritorna il valore di un'opzione a partire dalla sua sezione
+	 * e dal suo nome
+	 *
+	 * @param string  $option  settings field name
+	 * @param string  $section the section name this field belongs to
+	 * @param string  $default default text if it's not found
+	 * @return string
+	 */
+	public static function get_option( $option, $section, $default = '' ) {
+
+		$options = get_option( $section );
+
+		if ( isset( $options[$option] ) ) {
+			return $options[$option];
+		}
+
+		return $default;
+	}
 }
